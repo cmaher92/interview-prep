18a7723b186c8438c1f180b2654b8add818b7fcd
Connor Maher <connormmaher@gmail.com>
2016-03-28 23:40:54 -0700

    Added error handling for a TypeError in case the stock does not increase

--- interviewcake1.py
+++ interviewcake1.py
@@ -1,26 +1,23 @@
-yesterdays_stock_prices = [10, 7, 5, 8, 11, 7]
+yesterdays_stock_prices = [10, 7, 5, 8, 11, 9]
 
 def get_max_profit( yesterdays_stock_prices ):
     low_price = None
     high_price = None
+    max_profit = 0
+
     for price in yesterdays_stock_prices:
         if low_price == None or price < low_price:
             low_price = price
-            # print low_price
-
-    for price in yesterdays_stock_prices:
-        if high_price == None or price > high_price:
+        elif high_price == None or price > high_price:
             high_price = price
-            # print high_price
-            # 10, 11
 
-    max_profit = high_price - low_price
-    return max_profit
-
-    # for price in yesterdays_stock_prices:
-    #     if high_price == None or price > high_price:
-    #         high_price = price
-    #         max_profit = high_price - low_price
+    try:
+        max_profit = high_price - low_price
+    except TypeError:
+        max_profit = 0
+        return max_profit
+    else:
+        return max_profit
 
     # return max_profit
 max_profit = get_max_profit( yesterdays_stock_prices )
