f2e5139c0116c8b48357e3351fb5436fd383a1af
Connor Maher <connormmaher@gmail.com>
2016-03-29 11:44:17 -0700

    Completed with O(n) time & O(1) space

--- interviewcake1.py
+++ interviewcake1.py
@@ -1,24 +1,27 @@
 yesterdays_stock_prices = [10, 7, 5, 8, 11, 9]
 
-def get_max_profit( yesterdays_stock_prices ):
-    low_price = None
-    high_price = None
-    max_profit = 0
+def get_max_profit(yesterdays_stock_prices):
 
-    for price in yesterdays_stock_prices:
-        if low_price == None or price < low_price:
-            low_price = price
-        elif high_price == None or price > high_price:
-            high_price = price
+    # make sure that two prices
+    if len(yesterdays_stock_prices) > 2:
+        raise IndexError('Getting a profit requires at least two prices')
 
-    try:
-        max_profit = high_price - low_price
-    except TypeError:
-        max_profit = 0
-        return max_profit
-    else:
-        return max_profit
+    min_price = yesterdays_stock_prices[0]
+    max_profit = yesterdays_stock_prices[1] - yesterdays_stock_prices[0]
 
-    # return max_profit
+    for index, current_price in enumerate(yesterdays_stock_prices):
+        # enumerate returns [(0, 10), (1, 7)]
+        # so it starts at 1
+        if index == 0:
+            continue
+
+        potential profit = current_price - min_price
+
+        max_profit = max(max_profit, potential_profit)
+
+        min_price = min(min_price, current_price)
+
+return max_profit
+
 max_profit = get_max_profit( yesterdays_stock_prices )
 print max_profit
