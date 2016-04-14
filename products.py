2aea2fb7981728ea8b3be17853d0b2be48cd2f2b
Connor Maher <connormmaher@gmail.com>
2016-04-14 11:17:35 -0700

    Hour of practice

--- products.py
+++ products.py
@@ -1,6 +1,20 @@
-list = [1, 2, 3, 4]
+# 4/13  1 hour
+# 4/14  1 hour
 
-get_products_of_all_ints_except_at_index():
-# that takes a list of integers and returns a list of the products.
-# [24, 12, 8, 6]
-for index in list
+my_list = [1, 2, 3, 4]
+
+def get_products_of_all_ints_except_at_index(my_list):
+    new_list = []
+    for item in my_list:
+        for nested_item in my_list:
+            if item != nested_item:
+                item *= nested_item
+            else:
+        new_list.append(item)
+    print(new_list)
+
+
+print(get_products_of_all_ints_except_at_index(my_list))
+
+# use two loops to multiply the integer at every index by the integer at every
+# nested_index, unless index == nested_index.
